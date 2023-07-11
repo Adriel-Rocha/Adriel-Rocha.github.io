300d1c93a509acebbec575f6e6bea7c61d066db5
Adriel Francisco Rocha <67209277+Adriel-Rocha@users.noreply.github.com>
2023-07-11 01:59:29 -0300

    Improvements in JavaScript code

--- script/script.js
+++ script/script.js
@@ -1,6 +1,27 @@
+window.onload = function thisPageMenu() {
+    switch(document.title){
+        case 'Adriel':
+            menuHome.style.color = '#fff'
+            menuHome.style.fontWeight = '600'
+            break
+        case 'Projects':
+            menuProjects.style.color = '#fff'
+            menuProjects.style.fontWeight = '600'
+            break
+        case 'Contacts':
+            menuContacts.style.color = '#fff'
+            menuContacts.style.fontWeight = '600'
+            break
+        case 'About':
+            menuAbout.style.color = '#fff'
+            menuAbout.style.fontWeight = '600'
+            break
+    }
+}
+
 function resizedScreen() {
     if(window.innerWidth >= 480) {
-        itens.style.display = 'block'
+        itensMenu.classList.add('showMenu')
     }
     if(window.innerWidth >= 480 && window.innerWidth < 768) {
         adrielHomeImg.src = 'img/adriel-t-view.png'
@@ -10,11 +31,22 @@ function resizedScreen() {
 }
 
 function clickMenu() {
-    if(itens.style.display == 'block'){
-        itens.style.display = 'none'
+    
+    if(document.getElementsByClassName('showMenu').length){
+        itensMenu.classList.remove('showMenu')
     }else{
-        itens.style.display = 'block'
+        itensMenu.classList.add('showMenu')
     }
 }
 
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
 
+document.querySelectorAll('.hidden').forEach((element) => myObserver.observe(element))
